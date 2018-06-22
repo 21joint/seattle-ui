af08e839a29bec1f9bedd82c168ac922015f820c
prod1992 <levon1.grigoryan@gmail.com>
2018-06-23 02:38:35 +0400

    updates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
  * Application entry point
  */
 
-import './assets/styles/externals.scss';
 import './assets/styles/index.scss';
 import 'bootstrap-sass/assets/javascripts/bootstrap.min';
 import './partials/header/header';
