43efe73d95e6c0586255cb500ced91aa0af8d5a7
SAMVELHARUTYUNYAN <samvelharutyunyan777@gmail.com>
2018-06-22 04:35:54 +0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 /**
  * Application entry point
  */
+import 'bootstrap-sass/assets/javascripts/bootstrap/collapse';
 import './externals.scss';
-import './index.scss';
-
-
-jQuery.extend(jQuery.easing, {
-  easeNav (t) {
-    return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (-- t) * t * t * t * t;
-  }
-});
+import './index.scss'
