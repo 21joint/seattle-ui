be88113897a863d65455f36f21867808a4adf203
prod1992 <levon1.grigoryan@gmail.com>
2018-06-23 06:14:31 +0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 /**
  * Application entry point
  */
-
+import $ from 'jquery';
 import './assets/styles/index.scss';
 import 'bootstrap-sass/assets/javascripts/bootstrap.min';
 import './partials/header/header';
 import './partials/hero/hero';
+
+$('.checkout-single--venue').find('> .flex-row').addClass('align-items-center');
+$('.checkout-single--venue').each(function(index, venue) {
+  var $txtCol = $(venue).find('.checkout-venue--content > .flex-row > [class*=flex-col] p');
+  var $moreInfoBtn = $(venue).find('.btn-info--toggle');
+  $moreInfoBtn.insertAfter($(venue).find('.collapse'));
+  $(venue).find('.checkout-venue--info ul > li:first-child').prepend($txtCol);
+});
