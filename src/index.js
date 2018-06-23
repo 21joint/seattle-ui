ef78489a7063f3a093833d0a7ba7b55317cf028a
prod1992 <levon1.grigoryan@gmail.com>
2018-06-23 07:22:38 +0400

    updates

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap-sass/assets/javascripts/bootstrap.min';
 import './partials/header/header';
 import './partials/hero/hero';
 
-$('.checkout-single--venue').each(function(index, venue) {
+$('.checkout-single--venue').each(function (index, venue) {
   var $txtCol = $(venue).find('.checkout-venue--content > .flex-row > [class*=flex-col] p');
   var $newTxt = $('<li></li>').append($txtCol);
   var $moreInfoBtn = $(venue).find('.btn-info--toggle');
